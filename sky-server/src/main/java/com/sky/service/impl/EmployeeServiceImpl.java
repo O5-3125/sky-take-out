4e6d5da45c046b9cd3ec17c638518cfe1da553d4
O5-3125 <1971730645@qq.com>
2025-01-13 20:19:26 +0800

    数据库密码改为md5加密

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -38,8 +40,12 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
+
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 将明文密码进行md5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //对比md5密文
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
